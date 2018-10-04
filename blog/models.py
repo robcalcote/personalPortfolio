031b5591ca6a1207482dc23c8d754734a0c9cb7d
Rob <robcalcote@gmail.com>
2018-10-04 17:55:04 -0600

    added a blog page and migrated into DB

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    publish_date = models.DateField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+# Create a migration
+
+# Migrate
+
+# Add to the admin
