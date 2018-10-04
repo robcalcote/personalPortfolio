c773cef082cc5f4fb8b8638b5cb6e8f6adff195c
Rob <robcalcote@gmail.com>
2018-10-04 17:32:15 -0600

    migrated from sqlite to postgresql and changed database settings

--- personalPortfolio/settings.py
+++ personalPortfolio/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'personalPortfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
