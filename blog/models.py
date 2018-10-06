4bf1c8cb315ab6451331d90da90a9613dc0abe85
Rob <robcalcote@gmail.com>
2018-10-06 12:04:09 -0600

    Added a new function within blog/views.py named 'detail' to show the detailed body of a blog post when an int (matching pk of blog post) is put after the /blog/ URL

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,19 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
-    publish_date = models.DateField()
+    publish_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
-# Create a migration
+    # This function will return the title as the name of the blog post on the admin page
+    # rather than an arbitrary primary key that was previously shown
+    def __str__(self):
+        return self.title
 
-# Migrate
+    # This function will return only the first 100 characters of a blog post to avoid showing too much
+    def summary(self):
+        return self.body[:100]
 
-# Add to the admin
+    # This function will parse out a DateTimeField into only showing the Date in MMM DD, YYYY format
+    def publish_date_pretty(self):
+        return self.publish_date.strftime('%b %e, %Y')
